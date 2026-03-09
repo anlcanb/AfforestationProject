d4ff0b5bac7e29a133e10f29b01975ae64a7efbb
anlcanb <anlcanb>
2026-03-09 22:29:25 +0300

    Implement full CRUD operations for SitesController

--- Afforestation.API/Controllers/SitesController.cs
+++ Afforestation.API/Controllers/SitesController.cs
@@ -42,5 +42,40 @@ namespace Afforestation.API.Controllers
 
             return CreatedAtAction(nameof(GetById), new { id = site.Id }, site);
         }
+
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var site = await _context.Sites.FindAsync(id);          
+            if (site == null) return NotFound();
+
+            _context.Sites.Remove(site);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> Update(int id, [FromBody] Site updatedSite)
+        {
+            if (id != updatedSite.Id) return BadRequest();
+            var site = await _context.Sites.FindAsync(id);
+            if (site == null) return NotFound();
+            site.Name= updatedSite.Name;
+            site.Longitude= updatedSite.Longitude;
+            site.Latitude= updatedSite.Latitude;
+            site.Status= updatedSite.Status;
+            site.District= updatedSite.District;
+            site.PlantingData= updatedSite.PlantingData;
+            site.City= updatedSite.City;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
     }
+
 }
